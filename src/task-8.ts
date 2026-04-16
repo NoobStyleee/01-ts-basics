6b701d144234d9362c3d9e6e92694c751983e920
NoobStyle <mikledanse@mail.ru>
2026-04-16 14:59:11 +0200

    fixed task 8 promise

--- src/task-8.ts
+++ src/task-8.ts
@@ -7,12 +7,12 @@ interface Post {
 }
 
 
-async function fetchPosts(): Promise<Post[]> {
-  const response = await axios.get(
-    'https://jsonplaceholder.typicode.com/posts'
+const fetchPosts = async (): Promise<Post[]> => {
+  const response = await axios.get<Post[]>(
+    "https://jsonplaceholder.typicode.com/posts"
   );
   return response.data;
-}
+};
 
 fetchPosts().then((posts) => {
   console.log(posts[0].title);
